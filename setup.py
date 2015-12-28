89265bcbdc28dfba1cf9043d4102e667f95a80f3
Emanuel Feld <emanuelfeld@users.noreply.github.com>
2015-12-28 17:12:56 -0500

    Add to pypi setup script

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
-from setuptools import setup
+import poirot
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(name='poirot',
-	  version='0.1.0',
+	  version=poirot.__version__,
 	  description="Search a git repository's revision history for text patterns.",
 	  url='https://github.com/dcgov/poirot',
 	  author='Emanuel Feld',
 	  license='https://raw.githubusercontent.com/DCgov/poirot/master/LICENSE.md',
 	  packages=['poirot'],
-	  install_requires=['jinja2','nose','nose-progressive'],
+	  install_requires=['jinja2', 'tqdm'],
+	  test_suite='nose.collector',
+	  tests_require=['nose>=1.2.1', 'nose-progressive'],
+	  classifiers=[
+	  	'Environment :: Console',
+	  	'Intended Audience :: Developers',
+	  	'Programming Language :: Python'
+	  ],
+	  include_package_data=True,
 	  scripts=['bin/big-grey-cells', 'bin/little-grey-cells'],
 	  zip_safe=False)
