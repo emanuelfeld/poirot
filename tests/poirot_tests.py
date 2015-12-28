3412634d088bd7e6dfeb98b11031860c32783884
Emanuel Feld <emanuelfeld@users.noreply.github.com>
2015-12-28 17:10:02 -0500

    Adds basic testing

---
+++ tests/poirot_tests.py
@@ -0,0 +1,46 @@
+from poirot.poirot import Case, Poirot
+from poirot.clients import ConsoleClient
+from poirot.style import *
+from nose.tools import *
+from poirot.helpers import *
+
+
+def setUp():
+    global case, poirot
+    args = ['-u', 'git@github.com:DCgov/poirot-test-repo.git', '--revlist=all', '--dest=tests/fixtures', '--patterns=tests/test_patterns.txt', '--term', 'frabjous', '--skip']
+    case = Case(args)
+    case_parser_test()
+    client = ConsoleClient()
+    poirot = Poirot(client, case)
+    poirot.investigate()
+    find_matches_test()
+    style_color_test()
+
+
+def tearDown():
+    pass
+
+
+def case_parser_test():
+    eq_(len(case.__dict__), 12)
+    eq_(len(case.patterns), 8)
+    eq_(case.revlist, ['--all'])
+    eq_(case.git_url, 'git@github.com:DCgov/poirot-test-repo.git')
+
+
+def find_matches_test():
+    frabjous = poirot.findings['frabjous']
+    eq_(frabjous['f0a6ebc']['author_email'], 'emanuelfeld@users.noreply.github.com')
+    eq_(len(frabjous), 3)
+    eq_(len(frabjous['f0a6ebc']['files']), 2)
+    eq_(frabjous['f0a6ebc']['files'][0]['matches'][0]['line'], 12)
+    eq_(frabjous['f0a6ebc']['files'][1]['matches'][0]['line'], 2)
+
+    password = poirot.findings['password[ ]*=[ ]*.+']
+    eq_(len(password), 2)
+    ok_('log' in password['2f04563'].keys())
+
+
+def style_color_test():
+    for code in style_codes:
+        ok_(style("testing", code))
